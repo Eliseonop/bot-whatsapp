b3bc1ff0a06bee32c01159fce06bc34b0358dcb6
eliseodev <eliseonop@gmail.com>
2023-09-07 09:07:10 -0500

    update

--- utils/usuarios.js
+++ utils/usuarios.js
@@ -1,4 +1,15 @@
-const USUARIOS = []
+const USUARIOS = [
+  { name: 'Test Twilio', phone: 14155238886 },
+  { name: 'Christian Quevedo', phone: 51986395011 },
+  { name: 'Isaac Rivera', phone: 51967044833 },
+  { name: 'Marty Inga', phone: 51944274641 },
+  { name: 'Manuel Inga', phone: 51992554192 },
+  { name: 'José Antón', phone: 51996848333 },
+  { name: 'Mauro López', phone: 51933796895 },
+  { name: 'Robert Robles', phone: 51969334755 },
+  { name: 'Daniel Robles', phone: 51969334754 },
+  { name: 'Sergio Villa', phone: 51920187967 }
+]
 
 function verificarNumeroEnArray (numero) {
   for (const usuario of USUARIOS) {
